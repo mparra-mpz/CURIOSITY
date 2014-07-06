ec5e2ea7441dc8160ba99bc00bb79bbf8c45bc5c
mparra-mpz <mparra.mpz@gmail.com>
2014-07-06 02:41:00 -0400

    Add the basic CONTROL system class.

---
+++ CURIOSITY/src/Controller.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+
+import traceback
+from Joystick import Joystick
+from DeviceController import DeviceController
+from DeviceController import Device
+
+class Controller():
+    
+    def __init__(self):
+        '''
+        Initialize class attributes.
+        '''
+        self.joystick = Joystick()
+        self.communication = DeviceController()
+        self.gear = 0
+        
+    def initialize(self):
+        '''
+        Perform the joystick connection and get the bluetooth list.
+        '''
+        try:
+            self.joystick.connect()
+            self.communication.get_device_list()
+        except:
+            print str(traceback.format_exc())
+            
+    def clean_up(self):
+        '''
+        Perform the joystick and bluetooth disconnection.
+        '''
+        try:
+            self.joystick.disconnect()
+            self.communication.disconnect_device()
+        except:
+            print str(traceback.format_exc())
+    
+    def initialize_rover_communication(self, device):
+        '''
+        Initialize the bluetooth communication.
+        '''
+        try:
+            self.communication.connect_device(device.address)
+        except:
+            print str(traceback.format_exc())
+            
+    def send_rover_commands(self):
+        '''
+        Read the joystick commands and send it to the rover using a
+        bluetooth device.
+        '''
+        try:
+            command = ""
+            self.joystick.get_commands()
+            if self.joystick.commands["UP"] == True and \
+            self.joystick.commands["DOWN"] == False and \
+            self.joystick.commands["LEFT"] == False and \
+            self.joystick.commands["RIGHT"] == False:
+                command = "D1"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == True and \
+            self.joystick.commands["DOWN"] == False and \
+            self.joystick.commands["LEFT"] == False and \
+            self.joystick.commands["RIGHT"] == True:
+                command = "D2"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == False and \
+            self.joystick.commands["DOWN"] == False and \
+            self.joystick.commands["LEFT"] == False and \
+            self.joystick.commands["RIGHT"] == True:
+                command = "D3"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == False and \
+            self.joystick.commands["DOWN"] == True and \
+            self.joystick.commands["LEFT"] == False and \
+            self.joystick.commands["RIGHT"] == True:
+                command = "D4"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == False and \
+            self.joystick.commands["DOWN"] == True and \
+            self.joystick.commands["LEFT"] == False and \
+            self.joystick.commands["RIGHT"] == False:
+                command = "D5"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == False and \
+            self.joystick.commands["DOWN"] == True and \
+            self.joystick.commands["LEFT"] == True and \
+            self.joystick.commands["RIGHT"] == False:
+                command = "D6"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == False and \
+            self.joystick.commands["DOWN"] == False and \
+            self.joystick.commands["LEFT"] == True and \
+            self.joystick.commands["RIGHT"] == False:
+                command = "D7"
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["UP"] == True and \
+            self.joystick.commands["DOWN"] == False and \
+            self.joystick.commands["LEFT"] == True and \
+            self.joystick.commands["RIGHT"] == False:
+                command = "D8"
+                self.communication.send_instruction(command)
+                
+            if self.joystick.commands["SPEED"] == True and self.gear < 6:
+                self.gear = self.gear + 1
+                command = "V%d" % self.gear
+                self.communication.send_instruction(command)
+            elif self.joystick.commands["BREAK"] == True and self.gear > 0:
+                self.gear = self.gear - 1
+                command = "V%d" % self.gear
+                self.communication.send_instruction(command)
+        except:
+            print str(traceback.format_exc())
--- CURIOSITY/src/RoverWindow.py
+++ CURIOSITY/src/RoverWindow.py
@@ -11,31 +11,32 @@ def calculate(*args):
         speed.set(i)
     joystick_entry.insert(0, "USB")
     
-root = Tk()
-root.title("Robot Control Interface")
 
-mainframe = Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-Label(mainframe, text="Joystick").grid(column=1, row=1, sticky=W)
-joystick_entry = Entry(mainframe, width=15)
-joystick_entry.grid(column=2, row=1, sticky=(W, E))
-
-Label(mainframe, text="Bluetooth").grid(column=1, row=2, sticky=W)
-Combobox(mainframe).grid(column=2, row=2, sticky=W)
-
-Label(mainframe, text="Speed").grid(column=1, row=3, sticky=W)
-speed = Scale(mainframe, from_=0, to=6, orient=HORIZONTAL, length=175)
-speed.grid(column=2, row=3, sticky=W)
-
-Button(mainframe, text="Connect", command=calculate).grid(column=3, row=3, sticky=E)
-
-
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-joystick_entry.focus()
-root.bind('<Return>', calculate)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Robot Control Interface")
+    
+    mainframe = Frame(root, padding="3 3 12 12")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    mainframe.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
+    
+    Label(mainframe, text="Joystick").grid(column=1, row=1, sticky=W)
+    joystick_entry = Entry(mainframe, width=15)
+    joystick_entry.grid(column=2, row=1, sticky=(W, E))
+    
+    Label(mainframe, text="Bluetooth").grid(column=1, row=2, sticky=W)
+    Combobox(mainframe).grid(column=2, row=2, sticky=W)
+    
+    Label(mainframe, text="Speed").grid(column=1, row=3, sticky=W)
+    speed = Scale(mainframe, from_=0, to=6, orient=HORIZONTAL, length=175)
+    speed.grid(column=2, row=3, sticky=W)
+    
+    Button(mainframe, text="Connect", command=calculate).grid(column=3, row=3, sticky=E)
+    
+    for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+    
+    joystick_entry.focus()
+    root.bind('<Return>', calculate)
+    
+    root.mainloop()
