80297312120f39ffa5c1b4f2e958b79fa7ff4397
mparra-mpz <mparra.mpz@gmail.com>
2014-07-07 22:00:44 -0400

    Add arduino code and documentation readme.

---
+++ CURIOSITY/arduino/Bluetooth.ino
@@ -0,0 +1,47 @@
+
+#include <SoftwareSerial.h>
+
+#define RxD 10
+#define TxD 11
+#define RST 9
+#define LED 7
+
+SoftwareSerial BTSerial(RxD, TxD);
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(RST, OUTPUT);
+  digitalWrite(RST, LOW);
+  digitalWrite(LED, LOW);
+  digitalWrite(RST, HIGH);
+  
+  delay(500);
+  BTSerial.flush();
+  delay(500);
+  BTSerial.begin(9600);
+}
+
+void loop()
+{
+  char data;
+  if (BTSerial.available())
+  {
+    data = BTSerial.read();
+    if (data == 'D')
+    {
+      data = BTSerial.read();
+      if (data == '1') digitalWrite(LED, HIGH);
+      if (data == '3') digitalWrite(LED, HIGH);
+      if (data == '5') digitalWrite(LED, LOW);
+      if (data == '7') digitalWrite(LED, LOW);
+    } else {
+      if (data == 'V')
+      {
+        int gear = BTSerial.read() - '0';
+        if (gear > 3) digitalWrite(LED, HIGH);
+        if (gear < 3) digitalWrite(LED, LOW);
+      }
+    }
+  }
+}
