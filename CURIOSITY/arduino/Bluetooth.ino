4b01dc3478ab655cbe9135196daa296608f6d639
mparra-mpz <mparra.mpz@gmail.com>
2014-07-08 21:34:56 -0400

    Add logs for debug.

--- CURIOSITY/arduino/Bluetooth.ino
+++ CURIOSITY/arduino/Bluetooth.ino
@@ -20,6 +20,9 @@ void setup()
   BTSerial.flush();
   delay(500);
   BTSerial.begin(9600);
+  
+  Serial.begin(9600);
+  Serial.println("Start debugging");
 }
 
 void loop()
@@ -28,9 +31,11 @@ void loop()
   if (BTSerial.available())
   {
     data = BTSerial.read();
+    Serial.println(data);
     if (data == 'D')
     {
       data = BTSerial.read();
+      Serial.println(data);
       if (data == '1') digitalWrite(LED, HIGH);
       if (data == '3') digitalWrite(LED, HIGH);
       if (data == '5') digitalWrite(LED, LOW);
@@ -39,6 +44,7 @@ void loop()
       if (data == 'V')
       {
         int gear = BTSerial.read() - '0';
+        Serial.println(gear);
         if (gear > 3) digitalWrite(LED, HIGH);
         if (gear < 3) digitalWrite(LED, LOW);
       }
