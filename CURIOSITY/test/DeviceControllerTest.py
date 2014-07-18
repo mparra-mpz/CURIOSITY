0898052d93c513d7293975d0456559e82ef609cd
mparra-mpz <mparra.mpz@gmail.com>
2014-07-17 22:25:25 -0400

    Improve the test for bluetooth communication class.

--- CURIOSITY/test/DeviceControllerTest.py
+++ CURIOSITY/test/DeviceControllerTest.py
@@ -2,38 +2,38 @@
 
 import unittest
 import time
-from DeviceController import DeviceController
+from Communication import Communication
 
-class DeviceControllerTest(unittest.TestCase):
+class CommunicationTest(unittest.TestCase):
     
     def setUp(self):
         '''
         Verify environment is setup properly.
         '''
-        self.controller = DeviceController()
-        self.address = "00:12:12:28:10:49"
-        self.instruction = "Hello"
+        self.controller = Communication()
     
     def tearDown(self):
         '''
         Verify environment is tore down properly.
         '''
         pass
-    
-    def test_get_device_list(self):
+    def test_get_bluetooth_list(self):
         '''
-        Verify that the device list was completed without problems.
+        Verify that the bluetooth list was retrieve without problems.
         '''
-        value = self.controller.get_device_list()
+        value = False
+        b_list = self.controller.get_bluetooth_list()
+        if "Empty" not in b_list[0]:
+            value = True
         self.assertTrue(value)
         
     def test_send_instruction(self):
         '''
         Verify that the instruction was send without problems.
         '''
-        self.controller.connect_device(self.address)
-        value = self.controller.send_instruction(self.instruction)
-        time.sleep(5)
+        self.controller.connect_device("CURIOSITY")
+        value = self.controller.send_instruction("Hello")
+        time.sleep(3)
         self.controller.disconnect_device()
         self.assertTrue(value)
         
