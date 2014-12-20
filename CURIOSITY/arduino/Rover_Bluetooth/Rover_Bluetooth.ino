26df5e5f47ed07462ccf9e83a5da9d9666ae7484
mparra-mpz <mparra.mpz@gmail.com>
2014-12-20 15:10:55 -0300

    Modify the section structure to make compatible with Arduino sketch structure, modify all the rover sketch to use the library Pirate4WD.

---
+++ CURIOSITY/arduino/Rover_Bluetooth/Rover_Bluetooth.ino
@@ -0,0 +1,65 @@
+#include <SoftwareSerial.h>
+#include <Pirate4WD.h>
+
+#define E1 5
+#define M1 4
+#define E2 6
+#define M2 7
+#define RxD 10
+#define TxD 11
+#define MinVoltage 0
+
+SoftwareSerial btSerial(RxD, TxD);
+Pirate4WD rover(E1, M1, E2, M2);
+
+void setup()
+{
+  btSerial.flush();
+  delay(500);
+  btSerial.begin(9600);
+  
+  Serial.begin(9600);
+  Serial.println("Finish Arduino setup.");
+}
+
+int getVoltage(char data)
+{
+  int gear = data - '0';
+  int voltage = 0;
+  if (gear != 0) voltage = map(gear, 1, 6, MinVoltage, 255);
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+  return voltage;
+}
+
+void loop()
+{
+  char data;
+  if (btSerial.available())
+  {
+    data = btSerial.read();
+    Serial.println(data);
+    if (data == 'B')
+    {
+      data = btSerial.read();
+      Serial.println(data);
+      if (data == '0') rover.stop();
+    }
+    if (data == 'V')
+    {
+      data = btSerial.read();
+      Serial.println(data);
+      int voltage = getVoltage(data);
+      rover.setVoltage(voltage);
+    }
+    if (data == 'D')
+    {
+      data = btSerial.read();
+      Serial.println(data);
+      if (data == '1') rover.moveForward();
+      if (data == '3') rover.moveRight();
+      if (data == '5') rover.moveBack();
+      if (data == '7') rover.moveLeft();
+    }
+  }
+}
---
+++ CURIOSITY/arduino/Rover_Infrared/Rover_Infrared.ino
@@ -0,0 +1,73 @@
+#include <Pirate4WD.h>
+
+#define E1 5
+#define M1 4
+#define E2 6
+#define M2 7
+#define D0 11
+#define D1 10
+#define MinVoltage 0
+
+Pirate4WD rover(E1, M1, E2, M2);
+int cal_num = 1000;
+float cal_value_A0 = 0.0;
+float cal_value_A1 = 0.0;
+boolean is_black_A0 = false;
+boolean is_black_A1 = false;
+
+void setup()
+{
+  Serial.begin(9600); 
+  Serial.println("Start Arduino setup.");
+  
+  digitalWrite(D0, LOW);
+  digitalWrite(D1, LOW);
+  delay(500);
+  
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, HIGH);
+  for (int i = 0; i < cal_num; i++) {
+    cal_value_A0 = cal_value_A0 + analogRead(A0);
+    cal_value_A1 = cal_value_A1 + analogRead(A1);
+  }
+  cal_value_A0 = cal_value_A0 / cal_num;
+  cal_value_A1 = cal_value_A1 / cal_num;
+  Serial.print("Black A0: ");
+  Serial.print(cal_value_A0);
+  Serial.print(" -- Black A1: ");
+  Serial.println(cal_value_A1);
+  
+  Serial.println("Finish Arduino setup.");
+}
+
+void loop()
+{
+  rover.setVoltage(MinVoltage);
+  float value_A0 = analogRead(A0);
+  float value_A1 = analogRead(A1);
+  Serial.print("A0: ");  
+  Serial.print(value_A0);
+  Serial.print(" -- A1: ");  
+  Serial.println(value_A1);
+  
+  if (value_A0 < 0.75*cal_value_A0 || value_A0 > 1.25*cal_value_A0) {
+    is_black_A0 = false;
+    Serial.println("A0 is white");
+  } else {
+    is_black_A0 = true;
+    Serial.println("A0 is black");
+  }
+  
+  if (value_A1 < 0.75*cal_value_A1 || value_A1 > 1.25*cal_value_A1) {
+    is_black_A1 = false;
+    Serial.println("A1 is white");
+  } else {
+    is_black_A1 = true;
+    Serial.println("A1 is black");
+  }
+  
+  if (is_black_A0 == true && is_black_A1 == true) rover.moveForward();
+  else if (is_black_A0 == true && is_black_A1 == false) rover.moveRight();
+  else if (is_black_A0 == false && is_black_A1 == true) rover.moveLeft();
+  else if (is_black_A0 == false && is_black_A1 == false) rover.stop();
+}
