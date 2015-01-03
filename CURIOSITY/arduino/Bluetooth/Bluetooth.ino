a6acbcfe9c07ce3c3c566d961bcfc1f18ceb1557
mparra-mpz <mparra.mpz@gmail.com>
2015-01-03 19:48:16 -0300

    Update the simple bluetooh project documentation and code.

--- CURIOSITY/arduino/Bluetooth/Bluetooth.ino
+++ CURIOSITY/arduino/Bluetooth/Bluetooth.ino
@@ -2,9 +2,8 @@
 
 #define RxD 10
 #define TxD 11
-#define RST 9
+#define RST 8
 #define LED 7
-#define Delay 108
 
 SoftwareSerial BTSerial(RxD, TxD);
 
@@ -15,7 +14,6 @@ void setup()
   digitalWrite(RST, LOW);
   digitalWrite(LED, LOW);
   digitalWrite(RST, HIGH);
-  
   delay(500);
   BTSerial.flush();
   delay(500);
@@ -24,27 +22,17 @@ void setup()
 
 void loop()
 {
-  char data;
-  if (BTSerial.available())
+  if (BTSerial.available() > 0)
   {
-    delayMicroseconds(Delay);
-    data = BTSerial.read();
-    if (data == 'D')
+    String data = "";
+    char auxiliar;
+    while (BTSerial.available() > 0)
     {
-      delayMicroseconds(Delay);
-      data = BTSerial.read();
-      if (data == '1') digitalWrite(LED, HIGH);
-      if (data == '3') digitalWrite(LED, HIGH);
-      if (data == '5') digitalWrite(LED, LOW);
-      if (data == '7') digitalWrite(LED, LOW);
-    } else {
-      if (data == 'V')
-      {
-        delayMicroseconds(Delay);
-        int gear = BTSerial.read() - '0';
-        if (gear > 3) digitalWrite(LED, HIGH);
-        if (gear < 3) digitalWrite(LED, LOW);
-      }
+      auxiliar = BTSerial.read();
+      data += auxiliar;
     }
+    Serial.println(data);
+    if (data == "D1" or data == "D3") digitalWrite(LED, HIGH);
+    else if (data == "D5" or data == "D7") digitalWrite(LED, LOW);
   }
 }
